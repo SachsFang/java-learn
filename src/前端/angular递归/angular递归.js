b1499e2884e4020579ed268fecf7472699710a80
Sachs Fang <sachs.fang@cynovan.com>
2021-07-06 14:19:47 +0800

    git init

---
+++ src/前端/angular递归/angular递归.js
@@ -0,0 +1,35 @@
+$scope.categories = [
+    {
+        title: 'Computers',
+        categories: [
+            {
+                title: 'Laptops',
+                categories: [
+                    {
+                        title: 'Ultrabooks'
+                    },
+                    {
+                        title: 'Macbooks'
+                    }
+                ]
+            },
+            {
+                title: 'Desktops'
+            },
+            {
+                title: 'Tablets',
+                categories: [
+                    {
+                        title: 'Apple'
+                    },
+                    {
+                        title: 'Android'
+                    }
+                ]
+            }
+        ]
+    },
+    {
+        title: 'Printers'
+    }
+];
