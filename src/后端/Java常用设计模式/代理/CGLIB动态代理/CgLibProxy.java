fda08ff83d43edd132333ced15d4d7116bca1906
Sachs Fang <sachs.fang@cynovan.com>
2021-11-01 11:36:06 +0800

    CGLib动态代理补充

--- src/后端/Java常用设计模式/代理/CGLIB动态代理/CgLibProxy.java
+++
@@ -1,5 +0,0 @@
-package 后端.Java常用设计模式.代理.CGLIB动态代理;
-
-public class CgLibProxy {
-
-}
---
+++ src/后端/Java常用设计模式/代理/CGLIB动态代理/Test.java
@@ -0,0 +1,20 @@
+package 后端.Java常用设计模式.代理.CGLIB动态代理;
+
+import 后端.Java常用设计模式.代理.UserDaoImpl;
+
+/**
+ * Created by SachsFang on 2021/10/22 17:03
+ */
+/*
+public class Test {
+    // 创建代理对象
+    CglibProxy cglibProxy = new CglibProxy();
+    // 创建目标对象
+    UserDaoImpl userDaoImpl = new UserDaoImpl();
+    // 获取增强后的目标对象
+    UserDaoImpl userDao = (UserDaoImpl)cglibProxy.createProxy(userDaoImpl);
+    // 执行方法
+		userDao.addUser();
+		userDao.deleteUser();
+}
+*/
