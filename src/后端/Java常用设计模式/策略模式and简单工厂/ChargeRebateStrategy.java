32eb65d6c5a4c74728e1c2507671ebe075237f00
Shaobin <fangsb@tsintergy.com>
2022-03-14 12:49:09 +0800

    # 策略模式+简单工厂模式结合运用

---
+++ src/后端/Java常用设计模式/策略模式and简单工厂/ChargeRebateStrategy.java
@@ -0,0 +1,19 @@
+package 后端.Java常用设计模式.策略模式and简单工厂;
+
+import java.math.BigDecimal;
+
+/**
+ * @author shaobin
+ * @date 2022/3/14 11:56
+ */
+public class ChargeRebateStrategy extends ChargeStrategy {
+    private BigDecimal rebate;
+    public ChargeRebateStrategy(BigDecimal rebate) {
+        this.rebate = rebate;
+    }
+
+    @Override
+    BigDecimal calcResult(BigDecimal money) {
+        return money.multiply(rebate);
+    }
+}
---
+++ src/后端/Java常用设计模式/策略模式and简单工厂/ChargeReturnStrategy.java
@@ -0,0 +1,24 @@
+package 后端.Java常用设计模式.策略模式and简单工厂;
+
+import java.math.BigDecimal;
+
+/**
+ * @author shaobin
+ * @date 2022/3/14 11:43
+ */
+public class ChargeReturnStrategy extends ChargeStrategy {
+    private BigDecimal moneyCondition;
+    private BigDecimal moneyReturn;
+    public ChargeReturnStrategy(BigDecimal moneyCondition, BigDecimal moneyReturn) {
+        this.moneyCondition = moneyCondition;
+        this.moneyReturn = moneyReturn;
+    }
+    @Override
+    BigDecimal calcResult(BigDecimal money) {
+        BigDecimal result = money;
+        if (money.compareTo(moneyCondition) > -1) {
+            result = money.subtract(moneyReturn);
+        }
+        return result;
+    }
+}
