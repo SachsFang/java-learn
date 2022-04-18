a651faeded3bb31f94a0947b7157d98997d141f8
Shaobin <fangsb@tsintergy.com>
2022-04-18 12:09:56 +0800

    # 抽象工厂模式优化

---
+++ src/后端/Java常用设计模式/抽象工厂优化/抽象工厂and反射/Client.java
@@ -0,0 +1,14 @@
+package 后端.Java常用设计模式.抽象工厂优化.抽象工厂and反射;
+
+/**
+ * @author shaobin
+ * @date 2022/4/17 16:03
+ */
+public class Client {
+    public static void main(String[] args) {
+        DataAccess.getMenuDao().updateMenu();
+        DataAccess.getMenuDao().getMenu();
+        DataAccess.getUserDao().updateUser();
+        DataAccess.getUserDao().getUser();
+    }
+}
--- src/后端/Java常用设计模式/抽象工厂模式/database_example/Client.java
+++ src/后端/Java常用设计模式/抽象工厂模式/database_example/Client.java
@@ -17,5 +17,8 @@ public class Client {
         UserDao userDao = dataSourceFactory.getUserDao();
         userDao.updateUser();
         userDao.getUser();
+        /* 当需要进行拓展时，比如增加project，需要增加ProjectDao、OracleProjectDaoImpl、MySqlProjectDaoImpl
+        * 同时需要修改IDataSource、OracleFactory、MySqlFactory、不符合开闭原则
+        * 并且在没有框架的情况下，每次调用数据库方法时，都要写IDataSource dataSourceFactory = new MySqlFactory();，如果系统中有100个地方用到了， 就要改100个地方的new xxxFactory()才行，可以用简单工厂+抽象工厂进行优化*/
     }
 }
