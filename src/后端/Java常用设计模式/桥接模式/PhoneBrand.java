31debd042a4b8cda4973bc082b44a7940ee5756a
shaobin <fangsb@tsintergy.com>
2022-05-17 18:31:51 +0800

    # 完善桥接模式

--- src/后端/Java常用设计模式/桥接模式/PhoneBrand.java
+++ src/后端/Java常用设计模式/桥接模式/PhoneBrand.java
@@ -1,5 +1,10 @@
 package 后端.Java常用设计模式.桥接模式;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Objects;
+
 /**
  * 手机品牌抽象类
  * @author shaobin
@@ -7,9 +12,27 @@ package 后端.Java常用设计模式.桥接模式;
  */
 public abstract class PhoneBrand {
 
-    PhoneBrand(PhoneSoftWare phoneSoftWare) {
-        phoneSoftWare.run();
+    private List<PhoneSoftWare> phoneSoftWares = new ArrayList<>();
+
+    public PhoneBrand(List<PhoneSoftWare> phoneSoftWares) {
+        if (phoneSoftWares != null) {
+            this.phoneSoftWares = phoneSoftWares;
+        }
     }
 
+    public void showSoftware() {
+        this.phoneSoftWares.forEach(PhoneSoftWare::run);
+    }
 
+//    public void install(PhoneSoftWare phoneSoftWare) {
+//        if (Objects.nonNull(phoneSoftWare)) {
+//            phoneSoftWares.add(phoneSoftWare);
+//        }
+//    }
+//
+//    public void uninstall(PhoneSoftWare phoneSoftWare) {
+//        if (Objects.nonNull(phoneSoftWare)) {
+//            phoneSoftWares.remove(phoneSoftWare);
+//        }
+//    }
 }
--- src/后端/Java常用设计模式/桥接模式/XiaoMi.java
+++ src/后端/Java常用设计模式/桥接模式/XiaoMi.java
@@ -1,13 +1,14 @@
 package 后端.Java常用设计模式.桥接模式;
 
+import java.util.List;
+
 /**
  * @author shaobin
  * @date 2022/5/13 18:06
  */
 public class XiaoMi extends PhoneBrand {
 
-    XiaoMi(PhoneSoftWare phoneSoftWare) {
-        super(phoneSoftWare);
+    public XiaoMi(List<PhoneSoftWare> phoneSoftWares) {
+        super(phoneSoftWares);
     }
-
 }
