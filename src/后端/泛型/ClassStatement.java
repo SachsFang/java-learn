105649e46fa924f155367d6d395f391fe1dafdea
shaobin <fangsb@tsintergy.com>
2022-09-02 15:12:28 +0800

    # 泛型

---
+++ src/后端/泛型/ClassStatement.java
@@ -0,0 +1,32 @@
+package 后端.泛型;
+
+import 后端.lambdaAND函数式接口.UserEntity;
+
+import java.util.Collections;
+import java.util.List;
+
+/**
+ * <T> 在 class 中声明
+ * @author shaobin
+ * @date 2022/9/2 14:42
+ */
+public class ClassStatement<T> {
+    public static void main(String[] args) {
+        // ClassStatement<UserEntity> 指定了该实例泛型 T 为 UserEntity
+        ClassStatement<UserEntity> instance = new ClassStatement<>();
+        UserEntity userEntity = new UserEntity("fang", 18);
+        // 所以在该实例下只能传 UserEntity 对象
+        instance.getParam(userEntity);
+        instance.getParamList(Collections.singletonList(userEntity));
+    }
+
+    public T getParam(T object) {
+        System.out.println(object);
+        return object;
+    }
+
+    public List<T> getParamList(List<T> list) {
+        System.out.println(list);
+        return list;
+    }
+}
