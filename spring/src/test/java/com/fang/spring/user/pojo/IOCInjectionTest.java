28e1c37f126d677947f7ceb1fa97b475d0bdd2fd
shaobin <fangsb@tsintergy.com>
2022-09-09 18:02:24 +0800

    # spring bean 的学习 - 补充

--- spring/src/test/java/com/fang/spring/user/pojo/IOCInjectionTest.java
+++ spring/src/test/java/com/fang/spring/user/pojo/IOCInjectionTest.java
@@ -17,10 +17,14 @@ public class IOCInjectionTest {
 //        User constructInjectionParamUser = applicationContext.getBean("constructInjectionParamUser", User.class);
 //        System.out.println(constructInjectionParamUser + "生命周期：使用对象");
         // 属性注入IOC容器对象例子
-        UserServiceImpl userServiceImpl = applicationContext.getBean("userServiceImpl", UserServiceImpl.class);
-        userServiceImpl.addUser();
-        System.out.println(userServiceImpl.getMyUserDao().getName());
-        System.out.println(userServiceImpl);
+//        UserServiceImpl userServiceImpl = applicationContext.getBean("userServiceImpl", UserServiceImpl.class);
+//        userServiceImpl.addUser();
+//        System.out.println(userServiceImpl.getMyUserDao().getName());
+//        System.out.println(userServiceImpl);
+        // 自动装配例子
+        UserServiceImpl autoUserServiceImpl = applicationContext.getBean("autoUserServiceImpl", UserServiceImpl.class);
+        autoUserServiceImpl.addUser();
+        System.out.println(autoUserServiceImpl);
         // 关闭 spring 上下文
         applicationContext.close();
     }
