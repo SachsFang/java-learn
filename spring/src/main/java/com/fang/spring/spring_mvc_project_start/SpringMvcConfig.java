88ba2e49b8ef0b6dd1e03030f56465831d8b89c5
shaobin <fangsb@tsintergy.com>
2022-09-29 16:24:38 +0800

    # 【Spring】 整合JSP

--- spring/src/main/java/com/fang/spring/spring_mvc_project_start/SpringMvcConfig.java
+++ spring/src/main/java/com/fang/spring/spring_mvc_project_start/SpringMvcConfig.java
@@ -1,13 +1,48 @@
 package com.fang.spring.spring_mvc_project_start;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.DefaultServletHandlerConfigurer;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ViewResolverRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
 
 /**
  * @author shaobin
  * @date 2022/9/24 11:31
  */
 @Configuration
+@EnableWebMvc
 @ComponentScan("com.fang.spring.user")
-public class SpringMvcConfig {
+public class SpringMvcConfig implements WebMvcConfigurer {
+    /**
+     * 视图解析器配置
+     * @return
+     */
+    @Bean
+    public InternalResourceViewResolver resourceViewResolver() {
+        InternalResourceViewResolver internalResourceViewResolver = new InternalResourceViewResolver();
+        //请求视图文件的前缀地址
+        internalResourceViewResolver.setPrefix("/WEB-INF/jsp/");
+        //请求视图文件的后缀
+        internalResourceViewResolver.setSuffix(".jsp");
+        internalResourceViewResolver.setExposeContextBeansAsAttributes(true);
+        return internalResourceViewResolver;
+    }
+
+    /**
+     * 视图注册
+     *
+     * @param registry
+     */
+    public void configureViewResolvers(ViewResolverRegistry registry) {
+        registry.viewResolver(resourceViewResolver());
+    }
+
+    public void configureDefaultServletHandling(DefaultServletHandlerConfigurer configurer) {
+        // 激活配置
+        configurer.enable();
+    }
 }
---
+++ spring/src/main/java/com/fang/spring/user/controller/IndexController.java
@@ -0,0 +1,19 @@
+package com.fang.spring.user.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * @author shaobin
+ * @date 2022/9/29 16:18
+ */
+@Controller
+@RequestMapping("/index")
+public class IndexController {
+
+    @GetMapping()
+    public String toIndex() {
+        return "index";
+    }
+}
