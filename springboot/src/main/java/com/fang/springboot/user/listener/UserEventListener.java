69446403613fa6cfe505c841ab01d4f23524a457
shaobin <fangsb@tsintergy.com>
2022-10-09 18:39:03 +0800

    # 【springboot】异步调用方法 优化 - 补充

--- springboot/src/main/java/com/fang/springboot/user/listener/UserEventListener.java
+++ springboot/src/main/java/com/fang/springboot/user/listener/UserEventListener.java
@@ -12,7 +12,7 @@ import org.springframework.stereotype.Component;
 @Slf4j
 public class UserEventListener {
 
-    @Async("taskExecutor")
+    @Async("taskExecutor") // 当只配置了一个线程池时，"taskExecutor"可以不写，spring会默认找一个TaskExecutor
     public void sendSMS() throws InterruptedException {
         log.info("<3>向用户发送短信中...");
         Thread.sleep(3000);
