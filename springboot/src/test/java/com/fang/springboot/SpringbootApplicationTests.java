f5348838dc1dac505e0d4838eefd094ebefbe52e
shaobin <fangsb@tsintergy.com>
2022-10-20 17:00:16 +0800

    # 【springboot】Springboot 单元测试配置使用

--- springboot/src/test/java/com/fang/springboot/SpringbootApplicationTests.java
+++ springboot/src/test/java/com/fang/springboot/SpringbootApplicationTests.java
@@ -1,13 +1,23 @@
 package com.fang.springboot;
 
+import com.fang.springboot.common.config.CommonRestTemplateConfig;
+import com.fang.springboot.common.util.SpringContextManager;
+import com.fang.springboot.user.service.UserService;
+import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
+@Slf4j
 class SpringbootApplicationTests {
+
+    @Autowired
+    private UserService userService;
 
     @Test
     void contextLoads() {
+        log.info("输出结果：" + String.valueOf(userService.insertUser("f", 24)));
     }
 
 }
