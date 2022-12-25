66ed39a7e6cdea35de06e3dfde7c6f87616a6cb0
Sachs <1139110477@qq.com>
2022-12-25 23:16:58 +0800

    【micro-services】 init

---
+++ micro-services/service-api/src/main/java/com/fang/api/order/OrderService.java
@@ -0,0 +1,11 @@
+package com.fang.api.order;
+
+import com.fang.api.order.pojo.Order;
+
+/**
+ * @author shaobin
+ * @date 2022/12/25 18:55
+ */
+public interface OrderService {
+    Order getOrderInfo();
+}
---
+++ micro-services/service-api/src/main/java/com/fang/api/order/pojo/Order.java
@@ -0,0 +1,13 @@
+package com.fang.api.order.pojo;
+
+import lombok.Data;
+
+/**
+ * @author shaobin
+ * @date 2022/12/25 18:56
+ */
+@Data
+public class Order {
+    private String code;
+    private String remark;
+}
