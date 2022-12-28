45ee10ae3c58d4690775a89e748e16702afd0f33
shaobin <fangsb@tsintergy.com>
2022-12-28 17:00:16 +0800

    # 【micro-services】 使用WebService技术注释

--- micro-services/service-web/src/main/java/com/fang/web/WebRunApplication.java
+++ micro-services/service-web/src/main/java/com/fang/web/WebRunApplication.java
@@ -9,7 +9,7 @@ import org.springframework.context.annotation.ComponentScan;
  * @date 2022/12/26 10:17
  */
 @SpringBootApplication
-@ComponentScan({"com.fang.web", "com.fang.micro.api.order", "com.fang.micro.impl.order"})
+@ComponentScan({"com.fang.web", "com.fang.micro.api.order"})
 public class WebRunApplication {
     public static void main(String[] args) {
         SpringApplication springApplication = new SpringApplication(WebRunApplication.class);
--- micro-services/service-web/src/main/java/com/fang/web/controller/UserController.java
+++ micro-services/service-web/src/main/java/com/fang/web/controller/UserController.java
@@ -22,12 +22,13 @@ public class UserController {
     public static String serviceUrl = "http://user.impl.micro.fang.com/";
 
     /**
-     * WebServer技术实现RPC远程调用服务
+     * WebService技术实现RPC远程调用服务
      * @return
      * @throws MalformedURLException
      */
     @RequestMapping("/gerUserInfoByWebServer")
     public User getUserInfoByWebServer() throws MalformedURLException {
+        // 提供服务方通过发布wsdl文件注册到WebService目录，调用方服务通过WebService目录获取对应服务的wsdl文件信息进行服务的调用（http+xml的调用方式）
         URL url = new URL("http://127.0.0.1:8099/service/userServiceImpl?wsdl");
         QName serviceQName = new QName(serviceUrl, "UserServiceImplService");
         Service service = Service.create(url, serviceQName);
