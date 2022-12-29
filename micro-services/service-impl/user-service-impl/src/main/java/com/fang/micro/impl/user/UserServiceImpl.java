b3eeaba63d65760ad0a764566a2998ec4cffbd97
shaobin <fangsb@tsintergy.com>
2022-12-29 17:21:52 +0800

    # 【micro-services】 创建注册中心服务的生产者和消费者，并使用本地负载均衡

--- micro-services/service-impl/user-service-impl/src/main/java/com/fang/micro/impl/user/UserServiceImpl.java
+++ micro-services/service-impl/user-service-impl/src/main/java/com/fang/micro/impl/user/UserServiceImpl.java
@@ -2,7 +2,10 @@ package com.fang.micro.impl.user;
 
 import com.fang.micro.api.user.UserService;
 import com.fang.micro.api.user.pojo.User;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import javax.jws.WebService;
 
@@ -12,12 +15,20 @@ import javax.jws.WebService;
  */
 @Service
 @WebService
+@RestController //当此项目作为一个服务注册到注册中心时，需要通过RequestMapping访问对应的子服务
+@RequestMapping("/user")
 public class UserServiceImpl implements UserService {
+
+    @Value("${server.port}")
+    private String port;
+
     @Override
+    @RequestMapping("/getUserInfo")
     public User getUserInfo() {
         User user = new User();
         user.setId("01");
         user.setName("fang");
+        user.setPort(port);
         return user;
     }
 }
--- micro-services/service-web/src/main/java/com/fang/web/controller/UserController.java
+++ micro-services/service-web/src/main/java/com/fang/web/controller/UserController.java
@@ -2,14 +2,24 @@ package com.fang.web.controller;
 
 import com.fang.micro.api.user.UserService;
 import com.fang.micro.api.user.pojo.User;
+import com.fang.web.my_load_balance.LoadBalanceStrategy;
+import com.fang.web.my_load_balance.RandomLoadBalanceStrategy;
+import com.fang.web.my_load_balance.RoundLoadBalanceStrategy;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
+import org.springframework.cloud.client.loadbalancer.LoadBalancerClient;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
 import javax.xml.namespace.QName;
 import javax.xml.ws.Service;
 import java.net.MalformedURLException;
+import java.net.URI;
 import java.net.URL;
+import java.util.List;
 
 /**
  * @author shaobin
@@ -20,6 +30,33 @@ import java.net.URL;
 public class UserController {
 
     public static String serviceUrl = "http://user.impl.micro.fang.com/";
+
+    @Autowired
+    private DiscoveryClient discoveryClient;
+
+    @Autowired
+    private RestTemplate restTemplate;
+
+    @Autowired
+    private LoadBalancerClient loadBalancerClient;
+
+    @RequestMapping("getUerInfoByNacosProvider")
+    public User getUerInfoByNacosProvider() {
+        // 根据服务名称从服务中心获取接口地址集群列表
+        List<ServiceInstance> serviceInstanceList = discoveryClient.getInstances("user-micro-service");
+        /** 本地负载均衡 **/
+        // 使用自定义的负载均衡策略
+        LoadBalanceStrategy loadBalanceStrategy = new RoundLoadBalanceStrategy();
+//        LoadBalanceStrategy loadBalanceStrategy = new RandomLoadBalanceStrategy();
+//        ServiceInstance serviceInstance = loadBalanceStrategy.getSingleAddress(serviceInstanceList);
+        // 使用spring cloud封装好的LoadBalancerClient Ribbon负载均衡工具类
+        ServiceInstance serviceInstance = loadBalancerClient.choose("user-micro-service");
+        // 获取IP和端口
+        URI uri = serviceInstance.getUri();
+        /** 请求服务 **/
+        // 使用httpClient请求接口
+        return restTemplate.getForObject(uri + "/learn/user/getUserInfo", User.class);
+    }
 
     /**
      * WebService技术实现RPC远程调用服务
