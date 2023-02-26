3532bf5cf0492ac15541c34b0e61d6605b4f0458
shaobin <fangsb@tsintergy.com>
2023-02-26 10:33:19 +0800

    # 【rabbit-mq】rabbitmq重试的异常处理和保证幂等性

--- rabbit-mq/rabbitmq-consumer-one/src/main/java/com/fang/springboot/rabbitmq_consumer_one/ConsumerOne.java
+++ rabbit-mq/rabbitmq-consumer-one/src/main/java/com/fang/springboot/rabbitmq_consumer_one/ConsumerOne.java
@@ -3,11 +3,14 @@ package com.fang.springboot.rabbitmq_consumer_one;
 import com.fang.rabbitmq.pojo.User;
 import com.fang.springboot.rabbitmq_consumer_one.dao.OrderDAO;
 import com.fang.springboot.rabbitmq_consumer_one.pojo.Order;
+import com.rabbitmq.client.Channel;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.amqp.core.Message;
 import org.springframework.amqp.rabbit.annotation.RabbitHandler;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.transaction.annotation.Transactional;
 
 /**
  * @author shaobin
@@ -22,14 +25,22 @@ public class ConsumerOne {
     private OrderDAO orderDAO;
 
     @RabbitHandler
-    public void process(User user) {
-        log.info("消费者1接收消息成功：" + user);
-        Order order = new Order();
-        order.setCode("rabbitmq-test");
-        order.setAmount(1);
-        order.setProductId("001");
-        orderDAO.insertOrder(order);
-        int i = 1 / 0;
+    @Transactional // 保证幂等性
+    public void process(User user, Message message, Channel channel) {
+        try {// 此处是为了防止代码因为bug出错，为了让因为网络不通抖动等情况导致的消费失败进行重试
+            log.info("消费者1接收消息成功：" + user);
+            Order order = new Order();
+            order.setCode("rabbitmq-test");
+            order.setAmount(1);
+            order.setProductId("001");
+            orderDAO.insertOrder(order);
+//            int i = 1 / 0;
+            channel.basicAck(message.getMessageProperties().getDeliveryTag(), false);
+        } catch (Exception e) {
+            // 代码异常解决方案
+            // 1.可以将日志存放起来，后期通过定时任务或者人工的形式进行补偿
+            // 2.可以使用死信队列。在死信消费者中发布新的版本解决异常
+        }
     }
 
 }
