ed4f791440b83d1f8c7c1d08bca1edbecc1f63ad
shaobin <fangsb@tsintergy.com>
2023-08-21 09:43:03 +0800

    # 【springboot】 多线程服务（异步循环）注释

--- springboot/src/main/java/com/fang/springboot/common/service/MultiThreadService.java
+++ springboot/src/main/java/com/fang/springboot/common/service/MultiThreadService.java
@@ -12,11 +12,39 @@ import java.util.function.Function;
  * @date 2023/8/18 11:35
  */
 public interface MultiThreadService {
+    /**
+     * 集合异步循环
+     * @param taskList 循环任务列表
+     * @param function function
+     * @return 结果集合
+     */
     <T, R> List<R> asyncForEach(List<T> taskList, Function<T, R> function);
 
+    /**
+     * 集合异步循环
+     * @param taskList 循环任务列表
+     * @param function function
+     * @return 结果集合
+     * @param threadPoolIndex 线程池索引 解决父子任务在同一线程池引发的死锁问题
+     * @return 结果集合
+     */
     <T, R> List<R> asyncForEach(List<T> taskList, Function<T, R> function, Integer threadPoolIndex);
 
+    /**
+     * map集合异步循环
+     * @param taskMap 循环任务列表
+     * @param function function
+     * @return map结果集合
+     */
     <K, V, R> Map<K, R> asyncForEach(Map<K, V> taskMap, BiFunction<K, V, R> function);
 
+    /**
+     * 集合异步循环
+     * @param taskMap 循环任务列表
+     * @param function function
+     * @return map结果集合
+     * @param threadPoolIndex 线程池索引 解决父子任务在同一线程池引发的死锁问题
+     * @return map结果集合
+     */
     <K, V, R> Map<K, R> asyncForEach(Map<K, V> taskMap, BiFunction<K, V, R> function, Integer threadPoolIndex);
 }
