2f0f8a49c2cdd839d5382955ff7533cc857565bf
shaobin <fangsb@tsintergy.com>
2023-11-18 20:30:15 +0800

    # 【springboot】 整合liqiubase

--- sql/order.sql
+++ sql/order.sql
@@ -3,7 +3,7 @@ drop database if exists springboot_learn_data;
 create database springboot_learn_data;
 # 使用该数据库
 use springboot_learn_data;
-# 用户表
+# 订单表
 drop table if exists `order`;
 create table `order`
 (
